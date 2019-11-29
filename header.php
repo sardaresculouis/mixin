61b2653933c051807f0a6a74801abdb17e107c3d
Sardarescu Louis <louisbbty@gmail.com>
2019-11-29 02:04:24 +0200

    My message

--- header.php
+++ header.php
@@ -22,7 +22,6 @@
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'louis' ); ?></a>
 
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
@@ -36,16 +35,11 @@
 				?>
 				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
 				<?php
-			endif;
-			$louis_description = get_bloginfo( 'description', 'display' );
-			if ( $louis_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $louis_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?>
+
+			endif; ?>
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'louis' ); ?></button>
 			<?php
 			wp_nav_menu( array(
 				'theme_location' => 'menu-1',
